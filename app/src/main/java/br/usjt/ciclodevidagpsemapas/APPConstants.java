f5627df0383c5ac2674052d92dd30b01db9e1e64
Lucas Vale <lucasvale1998.lv@gmail.com>
2019-06-17 07:04:20 -0300

    Android

---
+++ app/src/main/java/br/usjt/ciclodevidagpsemapas/APPConstants.java
@@ -0,0 +1,14 @@
+/**
+ * Uriel Henrique Marques Farias Mishima RA: 81717300
+ */
+
+package br.usjt.ciclodevidagpsemapas;
+
+/*Lucas Do Vale Conceição
+RA:817116816
+ */
+
+public class APPConstants {
+    public static final long REQUEST_LOCATION_UPDATE_MIN_TIME = 2000;
+    public static final long REQUEST_LOCATION_UPDATE_MIN_DISTANCE = 200;
+}
